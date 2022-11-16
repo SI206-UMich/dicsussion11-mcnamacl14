d16fe3f3d4aab6cfd8c87f95255d2e0a68d9d116
Clare McNamara <clarem@Clares-MacBook-Air-2.local>
2022-11-16 14:11:55 -0500

    second commit

--- discussion11.py
+++ discussion11.py
@@ -39,8 +39,8 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    cur.execute("DROP TABLE IF EXISTS _____")
-    cur.execute("CREATE TABLE ____ __ TYPE ____")
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    cur.execute("CREATE TABLE Patients(pet_id INTEGER PRIMARY KEY, name TEXT, species_id INTEGER, age INTEGER, cuteness INTEGER, aggressiveness INTEGER)")
     conn.commit()
     
     pass
